5f6439acf8e95b69dee602dea1f137d48e1b4c89
Grzegorz Fabisiak <gmfabisiak@gmail.com>
2020-01-18 09:59:20 +0100

    Added a clean-up method

--- src/test/java/pl/gfabisiak/youtube/PageObjectTests.java
+++ src/test/java/pl/gfabisiak/youtube/PageObjectTests.java
@@ -1,5 +1,6 @@
 package pl.gfabisiak.youtube;
 
+import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -12,5 +13,10 @@ public class PageObjectTests {
     public void Initialize(){
         System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
         driver = new ChromeDriver();
+    }
+
+    @After
+    public void Finalize(){
+        driver.quit();
     }
 }
