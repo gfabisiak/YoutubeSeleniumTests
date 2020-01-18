a2bfe0c381823f7e05430f6b876842b126ac2cda
Grzegorz Fabisiak <gmfabisiak@gmail.com>
2020-01-18 10:03:29 +0100

    Added initial file for Youtube Main Page tests

---
+++ src/test/java/pl/gfabisiak/youtube/PageObjects/YoutubeMainPage.java
@@ -0,0 +1,9 @@
+package pl.gfabisiak.youtube.PageObjects;
+
+import org.openqa.selenium.WebDriver;
+
+public class YoutubeMainPage {
+    public WebDriver localDriver;
+
+    public String url = "https://youtube.com";
+}
