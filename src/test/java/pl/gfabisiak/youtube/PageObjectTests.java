f66819f60e02c407110b3a288e9eb6a34deea858
Grzegorz Fabisiak <gmfabisiak@gmail.com>
2020-01-18 09:57:59 +0100

    Added a set-up method

--- src/test/java/pl/gfabisiak/youtube/PageObjectTests.java
+++ src/test/java/pl/gfabisiak/youtube/PageObjectTests.java
@@ -1,8 +1,16 @@
 package pl.gfabisiak.youtube;
 
+import org.junit.Before;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class PageObjectTests {
 
     public WebDriver driver;
+
+    @Before
+    public void Initialize(){
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+        driver = new ChromeDriver();
+    }
 }
